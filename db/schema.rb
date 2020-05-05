0a4d71f8156d6c783211e3c3a45c5d8b27e34c4d
OKURA Masafumi <masafumi.o1988@gmail.com>
2020-05-05 19:24:34 +0900

    Generate User model
    - `rails g model user name email`
    - Add non-null constraints to name and email column
    - Add presence and uniqueness validation to name and email attribute
    - `rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_102202) do
+ActiveRecord::Schema.define(version: 2020_05_05_102324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
